d76ae8702d0746ce1f59d570614c46cc73553345
spuxx1701 <accounts@spuxx.dev>
2024-05-23 09:51:18 +0200

    chore: PageContent can now have a title

--- src/components/app/PageContent.vue
+++ src/components/app/PageContent.vue
@@ -2,25 +2,52 @@
 import { useRoute } from 'vue-router';
 import { VScrollXTransition } from 'vuetify/components';
 
+defineProps<{
+  title?: string;
+}>();
+
 const route = useRoute();
 </script>
 
 <template>
   <Teleport to="#main-content">
     <VScrollXTransition appear hide-on-leave>
-      <div id="page-content" :key="route.path" class="pa-2 pa-sm-4 pa-md-8">
+      <div :key="route.path" class="content pa-2 pa-sm-4 pa-md-8">
+        <header v-if="title" class="header">
+          <h1>
+            {{ title }}
+          </h1>
+          <hr />
+        </header>
         <slot></slot>
       </div>
     </VScrollXTransition>
   </Teleport>
 </template>
 
-<style>
-#page-content {
+<style scoped>
+.content {
   box-sizing: border-box;
   width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.header {
+  margin-bottom: 1rem;
+  min-width: 50%;
+  max-width: 100%;
+  h1 {
+    margin: 0;
+    text-align: center;
+  }
+
+  hr {
+    height: 2px;
+    border: none;
+    background-color: rgb(var(--v-border-color), var(--v-border-opacity));
+    border-radius: 50%;
+  }
 }
 </style>
