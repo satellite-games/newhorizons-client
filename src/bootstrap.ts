d3ae01094fe7d69d7c3b861f9e349405394d58c8
spuxx1701 <accounts@spuxx.dev>
2024-05-23 17:37:53 +0200

    chore: Rename 'informatics' to 'information-technology'

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -2,11 +2,12 @@ import { Config, Intl, Logger } from '@spuxx/browser-utils';
 import { appConfig, type AppConfig } from './config/app.config';
 import deApplication from '@/assets/locales/de.yaml';
 import { de as deBlueprints } from '@newhorizons/core/locales';
+import { de as deWiki } from '@newhorizons/wiki/locales';
 
 Config.setup<AppConfig>({ defaultConfig: appConfig });
 Logger.setLevel(Config.getConfig<AppConfig>().LOG_LEVEL);
 
-const de = { ...deApplication, ...deBlueprints };
+const de = { ...deApplication, ...deBlueprints, ...deWiki };
 Intl.setup({
   fallbackLocale: Config.getConfig<AppConfig>().DEFAULT_LOCALE,
   dictionaries: [
