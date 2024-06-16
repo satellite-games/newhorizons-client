c3a31e52220cd3293c5050ff77e73c70eb29b245
spuxx1701 <accounts@spuxx.dev>
2024-06-16 23:57:35 +0200

    chore: Minor improvements to `DebugContent` component

--- src/components/display/DebugContent.vue
+++ src/components/display/DebugContent.vue
@@ -2,37 +2,46 @@
 import type { AppConfig } from '@/config/app.config';
 import { Icon } from '@iconify/vue';
 import { Config } from '@spuxx/browser-utils';
-import { VCard } from 'vuetify/components';
+import { VExpansionPanel, VExpansionPanels } from 'vuetify/components';
 
 const display = Config.getConfig<AppConfig>().LOG_LEVEL === 'debug';
 </script>
 
 <template>
-  <VCard v-if="display" class="debug-content" elevation="24">
-    <p class="header">
-      <Icon class="icon" icon="mdi:code"></Icon>
-      Debug
-    </p>
-    <pre><slot></slot></pre>
-  </VCard>
+  <VExpansionPanels variant="popout">
+    <VExpansionPanel v-if="display" class="debug-content" elevation="24">
+      <template v-slot:title>
+        <h3 class="header">
+          <Icon class="icon" icon="mdi:code"></Icon>
+          Debug
+        </h3>
+      </template>
+      <template v-slot:text>
+        <pre><slot></slot></pre>
+      </template>
+    </VExpansionPanel>
+  </VExpansionPanels>
 </template>
 
 <style scoped>
 .debug-content {
   min-width: 10rem;
-  max-width: 100%;
+  width: fit-content;
+  /* max-width: 100%; */
   padding: 0.5rem;
   margin: 1rem;
   border-radius: var(--v-border-radius);
-  background-color: rgb(var(--v-theme-warning), 0.5);
+  background: linear-gradient(
+    90deg,
+    rgb(var(--v-theme-primary), 0.5),
+    rgb(var(--v-theme-accent), 0.5)
+  );
   font-family: monospace;
 }
 
 .header {
+  margin: 0;
   display: flex;
-  align-items: center;
-  border-bottom: 1px solid rgb(var(--v-border-color), var(--v-border-opacity));
-  margin-bottom: 0.5rem;
 }
 
 .icon {
